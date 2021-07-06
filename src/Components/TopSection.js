eca408e6306ae95ae01adca166b1e05160834b57
rohitmehta6 <iamakarenite@gmail.com>
2021-07-06 17:29:56 +0530

    code changes

--- src/Components/TopSection.js
+++ src/Components/TopSection.js
@@ -8,7 +8,7 @@ const TopSection = () => {
         <div className="container">
         <div className="left-container">
             <h1>Let's build the future of design, now.</h1>
-            <p>Create amazing tools and integrations for 1 million users or customise Marvel for your team</p>
+            <p>Create amazing tools and integrations for 1 million users or customise Marvel for your team.</p>
             <button>Submit your app</button>
             <button>View API Docs</button>
             
